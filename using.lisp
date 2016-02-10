ee7f87f8d248659e86d718e5e136472f9147060b
Zach Kost-Smith <zachkostsmith@gmail.com>
2016-02-10 10:36:08 -0600

    Change label-nc to flet-nc
    Noticed that the semantics are actually like flet, not labels.

--- using.lisp
+++ using.lisp
@@ -1,18 +1,18 @@
 
 (in-package :using)
 
-(defmacro label-nc ((name args &body fn-body) &body body)
-  "Like LABELS but only takes one definition and doesn't close over any
-variables in the environment."
+(defmacro flet-nc ((name args &body fn-body) &body body)
+  "Like FLET but only takes one definition and doesn't close over any variables
+in the environment."
   (let ((fn (gensym (symbol-name name)))
         (rest-sym (gensym)))
     `(let ((,fn ,(eval `(lambda ,args ,@fn-body))))
-       (labels ((,name (&rest ,rest-sym) (apply ,fn ,rest-sym)))
+       (flet ((,name (&rest ,rest-sym) (apply ,fn ,rest-sym)))
          ,@body))))
 
 (defmacro using (args &body body)
   "Execute BODY in a context where only the variables within the argument list
 and special bindings are in effect."
   (let ((fn (gensym)))
-    `(label-nc (,fn ,args ,@body)
+    `(flet-nc (,fn ,args ,@body)
        (,fn ,@args))))
